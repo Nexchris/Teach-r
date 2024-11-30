1ed22a5529b71375649bcd15f3fc892d1ca8ba6b
NexChris <ngabalachris@gmail.com>
2024-11-30 22:36:55 +0100

    sqlite database init

--- symfony/src/Controller/ProductController.php
+++ symfony/src/Controller/ProductController.php
@@ -1,18 +1,24 @@
 <?php
 
+// src/Controller/ProductController.php
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/', name: 'product_index', methods: ['GET'])]
+    public function index(ProductRepository $productRepository): Response
     {
+        // Récupérer les produits depuis la base de données
+        $products = $productRepository->findAll();
+
+        // Rendre la vue avec les produits
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 }
