b12a38fc290374a55c28a6b4e626a404dc99824a
NexChris <ngabalachris@gmail.com>
2024-11-30 19:48:41 +0100

    Create Category and Product entity with properties

---
+++ symfony/src/Entity/Product.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    
+    #[ORM\Column(type: "string")]
+    private string $name; 
+    
+    #[ORM\Column(type: "text", nullable: true)]
+    private ?string $description = null; 
+    
+    #[ORM\Column(type: "float")]
+    private float $price; 
+    
+    #[ORM\Column(type: "datetime")]
+    private \DateTime $creationDate; 
+
+    #[ORM\ManyToOne(targetEntity: Category::class)]  
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;  
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function getCreationDate(): \DateTime
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTime $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+        return $this;
+    }
+
+    public function getCategory(): ?Category  
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self  
+    {
+        $this->category = $category;
+        return $this;
+    }
+}
--- symfony/src/Repository/CategoryRepository.php
+++ symfony/src/Repository/CategoryRepository.php
@@ -2,39 +2,39 @@
 
 namespace App\Repository;
 
-use App\Entity\Produits;
+use App\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Produits>
+ * @extends ServiceEntityRepository<Category>
  */
-class ProduitsRepository extends ServiceEntityRepository
+class CategoryRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Produits::class);
+        parent::__construct($registry, Category::class);
     }
 
 //    /**
-//     * @return Produits[] Returns an array of Produits objects
+//     * @return Category[] Returns an array of Category objects
 //     */
 //    public function findByExampleField($value): array
 //    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
 //            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
+//            ->orderBy('c.id', 'ASC')
 //            ->setMaxResults(10)
 //            ->getQuery()
 //            ->getResult()
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?Produits
+//    public function findOneBySomeField($value): ?Category
 //    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
 //            ->setParameter('val', $value)
 //            ->getQuery()
 //            ->getOneOrNullResult()
